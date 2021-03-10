6564d73867eb9ead5410efcb2046077e81ce0c99
Gonzalo Cáceres <47033752+GonzaloMCG@users.noreply.github.com>
2021-03-10 19:07:09 -0300

    prueba2
    prueba2

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -179,7 +179,7 @@ export default class Preloader extends Phaser.Scene
     });
     this.anims.create({ key: 'equipo1avion1AumentarAltura', frames: this.frameNames, frameRate: 24, repeat: 0 });
 
-//vistaLateralanimacionAumentarAlturaConPocaVida --- SEGUIR DESDE ACA
+//vistaLateralanimacionAumentarAlturaConPocaVida ---  SEGUIR DESDE ACA
 
 //vistaLateralanimacionDisminuirAltura
     this.frameNames = this.anims.generateFrameNames('equipo1avion1', {
